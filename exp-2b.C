065d5a8716be0636b88a41bf04f32612f742fd31
anasshaikh2006 <anasshaikh05@eng.rizvi.edu.in>
2025-12-02 15:24:02 +0530

    Add C program to sum odd numbers in a range

---
+++ exp-2b.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, i, sum = 0;
+ printf("Enter the starting number: ");
+    scanf("%d", &start);
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+  for(i = start; i <= end; i++) {
+        if(i % 2 != 0) {  
+            sum += i;    
+        }
+    }
+
+    printf("Sum of all odd numbers between %d and %d is: %d\n", start, end, sum);
+
+    return 0;
+}
