18bcda94607e8f9a9d7fd399965d6ab94f611dc2
anasshaikh2006 <anasshaikh05@eng.rizvi.edu.in>
2025-12-02 15:14:07 +0530

    Add exp-10a.C for summing two numbers using pointers

---
+++ exp-10a.C
@@ -0,0 +1,21 @@
+/* anas shaikh
+251M005
+mech
+f*/
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *p1, *p2;
+printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+ p1 = &a;
+    p2 = &b;
+  sum = *p1 + *p2;
+
+    printf("Sum of %d and %d is: %d\n", *p1, *p2, sum);
+
+    return 0;
+}
