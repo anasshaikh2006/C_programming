d240c4ae99b2fe5f7b2cc7cad86c1d65e24f12f6
anasshaikh2006 <anasshaikh05@eng.rizvi.edu.in>
2025-12-02 15:26:10 +0530

    Add basic details program in exp-1a.C

---
+++ exp-1a.C
@@ -0,0 +1,23 @@
+/* anas shaikh
+251M005
+mech
+f*/
+#include <stdio.h>
+
+int main() {
+    char name[50] = "John Doe";
+    int age = 25;
+    char gender = 'M';
+    char city[50] = "New York";
+
+    printf("----- Basic Details -----\n\n");
+
+    printf("Name:\t%s\n", name);      
+    printf("Age:\t%d\n", age);
+    printf("Gender:\t%c\n", gender);
+    printf("City:\t%s\n", city);
+
+    printf("\n-------------------------\n");
+
+    return 0;
+}
